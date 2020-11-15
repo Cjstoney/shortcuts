359ec255c30ec3abdcee2d512b1821916f122738
Camerone Stoney <Cameronestoney@yahoo.com>
2020-11-15 09:52:58 -0800

    chore: added shortcuts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import macShortcuts from '../assets/macShortcuts.json';
+import windowsShortcuts from '../assets/windowsShortcuts.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shortcutHelper';
+
+macShortcuts = macShortcuts;
+
+
+
 }
