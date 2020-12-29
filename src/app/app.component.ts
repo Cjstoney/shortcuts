d0e118c4fffc2d858fe1f5e94108d3d42ac3f64b
Camerone Stoney <Cameronestoney@yahoo.com>
2020-12-29 10:25:13 -0800

    refactor: auto complete input added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core'
 import macShortcuts from '../assets/macShortcuts.json'
 import windowsShortcuts from '../assets/windowsShortcuts.json'
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {startWith, map} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'shortcutHelper'
   typeDisplay: InfoObject[] = [
     {keys: '',
@@ -17,15 +21,33 @@ export class AppComponent {
   }
 ]
 
-  macShortcuts: InfoObject[] = macShortcuts
-  windowsShortcuts: InfoObject[] = windowsShortcuts
+macShortcuts: InfoObject[] = macShortcuts
+windowsShortcuts: InfoObject[] = windowsShortcuts
+inputString: string[] = []
+control = new FormControl()
+filteredShortCuts: Observable<string []>
+
+ngOnInit(): void {
+  this.filteredShortCuts = this.control.valueChanges.pipe(
+    startWith(''),
+    map(value => this._filter(value))
+  );
+}
+
 
   clickedButton(event) {
     event === 'Mac' ?
     this.typeDisplay = macShortcuts : this.typeDisplay = windowsShortcuts
   }
 
+  private _filter(value: string): string[] {
+    const filterValue = this._normalizeValue(value);
+    return this.typeDisplay.map(x=>x.description).filter(shortcutDescription => this._normalizeValue(shortcutDescription).includes(filterValue));
+  }
 
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
 
 }
 
