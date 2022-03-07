1504959fe1b0bf8e031d400a584c19b80ecf0934
Camerone Stoney <Cameronestoney@yahoo.com>
2022-03-06 16:05:58 -0800

    chore: router started for each view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core'
 import { MatAutocompleteModule} from '@angular/material/autocomplete'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import {MatIconModule} from '@angular/material'
-
-
-import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListViewComponent } from './list-view/list-view.component';
 import { SearchViewComponent } from './search-view/search-view.component'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [
@@ -19,7 +17,10 @@ import { SearchViewComponent } from './search-view/search-view.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'list-view',component:ListViewComponent},
+      {path:'search-view', component:SearchViewComponent}
+    ]),
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
